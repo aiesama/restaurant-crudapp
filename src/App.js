69862a66ec5a84538cfa624baa6de4d56eece102
aiesama <floreslalaine8@gmail.com>
2024-06-14 01:27:29 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -70,212 +70,246 @@ function App() {
 
   return (
     <React.Fragment>
-      {/* header */}
-      <div className="Top">
-        <label className="Input-Text">RESTAURANT</label>
-      </div>
-
-      {/* body */}
       <div className="App">
-        {/* category text input */}
-        <div className="Input">
-          <label>Category:</label>
-          <input
-            required
-            type="text"
-            className="Input-Text"
-            onChange={(e) =>
-              setInputValue({ ...inputvalue, category: e.target.value })
-            }
-          />
-        </div>
-
-        {/* name text input */}
-        <div className="Input">
-          <label>Name:</label>
-          <input
-            required
-            type="text"
-            className="Input-Text"
-            onChange={(e) =>
-              setInputValue({ ...inputvalue, name: e.target.value })
-            }
-          />
-        </div>
-
-        {/* size choose input */}
-        <div className="Input">
-          <label>Size:</label>
-          <select
-            onChange={(e) =>
-              setInputValue({ ...inputvalue, size: e.target.value })
-            }
-            className="Input-Text"
-          >
-            <option> N/A </option>
-            <option> Small </option>
-            <option> Medium </option>
-            <option> Large </option>
-          </select>
-        </div>
-
-        {/* price input */}
-        <div className="Input">
-          <label>Price:</label>
-          <input
-            required
-            type="number"
-            className="Input-Text"
-            onChange={(e) =>
-              setInputValue({ ...inputvalue, price: e.target.value })
-            }
-          />
-        </div>
-
-        {/* cost input */}
-        <div className="Input">
-          <label>Cost:</label>
-          <input
-            required
-            type="number"
-            className="Input-Text"
-            onChange={(e) =>
-              setInputValue({ ...inputvalue, cost: e.target.value })
-            }
-          />
-        </div>
-
-        {/* amount in stock input */}
-        <div className="Input">
-          <label>Amount in Stock:</label>
-          <input
-            required
-            type="number"
-            className="Input-Text"
-            onChange={(e) =>
-              setInputValue({ ...inputvalue, amountinstock: e.target.value })
-            }
-          />
-        </div>
-
-        <button onClick={writeToDatabase}> Submit </button>
-      </div>
-
-      {/* Get Data from Firebase */}
-      <div className="Top">
-        <label className="Input-Text">Table</label>
-        <div>
-          <table>
-            <th>Category</th>
-            <th>Name</th>
-            <th>Size</th>
-            <th>Price</th>
-            <th>Cost</th>
-            <th>Amount in Stock</th>
-
-            {readvalue?.map((data) => (
-              <tr>
-                <td>{data.category}</td>
-                <td>{data.name}</td>
-                <td>{data.size}</td>
-                <td>{data.price}</td>
-                <td>{data.cost}</td>
-                <td>{data.amountinstock}</td>
-                <td>
-                  <button
-                    onClick={() =>
-                      setMagic({
-                        ID: data.ID,
-                        category: data.category,
-                        name: data.name,
-                        size: data.size,
-                        price: data.price,
-                        cost: data.cost,
-                        amountinstock: data.amountinstock,
-                      })
-                    }
-                  >
-                    Update
-                  </button>
-                </td>
-                <td>
-                  <button onClick={() => handleDelete(data.ID)}>Delete</button>
-                </td>
-              </tr>
-            ))}
-          </table>
-        </div>
-      </div>
-
-      {/* For Data Update */}
-      {magic && (
+        {/* header */}
         <div className="Top">
-          <button onClick={() => setMagic(null)}>Close</button>
-
-          <div className="Input">
-            <label>Category</label>
-            <input
-              type="text"
-              value={magic.category}
-              onChange={(e) => setMagic({ ...magic, category: e.target.value })}
-            />
-          </div>
-
-          <div className="Input">
-            <label>Name</label>
-            <input
-              type="text"
-              value={magic.name}
-              onChange={(e) => setMagic({ ...magic, name: e.target.value })}
-            />
-          </div>
-
-          <div className="Input">
-            <label>Size</label>
-            <select
-              value={magic.size}
-              onChange={(e) => setMagic({ ...magic, size: e.target.value })}
-              className="Input-Text"
-            >
-              <option> N/A </option>
-              <option> Small </option>
-              <option> Medium </option>
-              <option> Large </option>
-            </select>
-          </div>
-
-          <div className="Input">
-            <label>Price</label>
-            <input
-              type="text"
-              value={magic.price}
-              onChange={(e) => setMagic({ ...magic, price: e.target.value })}
-            />
-          </div>
-
-          <div className="Input">
-            <label>Cost</label>
-            <input
-              type="text"
-              value={magic.cost}
-              onChange={(e) => setMagic({ ...magic, cost: e.target.value })}
-            />
-          </div>
-
-          <div className="Input">
-            <label>Amount in Stock</label>
-            <input
-              type="text"
-              value={magic.amountinstock}
-              onChange={(e) =>
-                setMagic({ ...magic, amountinstock: e.target.value })
-              }
-            />
-          </div>
-
-          <button onClick={() => updateToDatabase(magic)}> Update </button>
+          <label className="Top-Label">RESTAURANT CRUD SYSTEM</label>
         </div>
-      )}
+
+        {/* body */}
+        <div className="App">
+          <div className="All-Input-Area">
+            {/* category text input */}
+            <div className="Input">
+              <label className="Input-Label">Category:</label>
+              <input
+                required
+                type="text"
+                className="Input-Text"
+                onChange={(e) =>
+                  setInputValue({ ...inputvalue, category: e.target.value })
+                }
+              />
+            </div>
+
+            {/* name text input */}
+            <div className="Input">
+              <label className="Input-Label">Name:</label>
+              <input
+                required
+                type="text"
+                className="Input-Text"
+                onChange={(e) =>
+                  setInputValue({ ...inputvalue, name: e.target.value })
+                }
+              />
+            </div>
+
+            {/* size choose input */}
+            <div className="Input">
+              <label className="Input-Label">Size:</label>
+              <select
+                onChange={(e) =>
+                  setInputValue({ ...inputvalue, size: e.target.value })
+                }
+                className="Input-Text"
+              >
+                <option> N/A </option>
+                <option> Small </option>
+                <option> Medium </option>
+                <option> Large </option>
+              </select>
+            </div>
+
+            {/* price input */}
+            <div className="Input">
+              <label className="Input-Label">Price:</label>
+              <input
+                required
+                type="number"
+                className="Input-Text"
+                onChange={(e) =>
+                  setInputValue({ ...inputvalue, price: e.target.value })
+                }
+              />
+            </div>
+
+            {/* cost input */}
+            <div className="Input">
+              <label className="Input-Label">Cost:</label>
+              <input
+                required
+                type="number"
+                className="Input-Text"
+                onChange={(e) =>
+                  setInputValue({ ...inputvalue, cost: e.target.value })
+                }
+              />
+            </div>
+
+            {/* amount in stock input */}
+            <div className="Input">
+              <label className="Input-Label">Amount in Stock:</label>
+              <input
+                required
+                type="number"
+                className="Input-Text"
+                onChange={(e) =>
+                  setInputValue({
+                    ...inputvalue,
+                    amountinstock: e.target.value,
+                  })
+                }
+              />
+            </div>
+
+            <button className="Button" onClick={writeToDatabase}>
+              {" "}
+              Submit{" "}
+            </button>
+          </div>
+        </div>
+
+        {/* Get Data from Firebase */}
+        <div className="Body">
+          <div className="All-Input-Area">
+            <table>
+              <th>Category</th>
+              <th>Name</th>
+              <th>Size</th>
+              <th>Price</th>
+              <th>Cost</th>
+              <th>Amount in Stock</th>
+
+              {readvalue?.map((data) => (
+                <tr>
+                  <td>{data.category}</td>
+                  <td>{data.name}</td>
+                  <td>{data.size}</td>
+                  <td>{data.price}</td>
+                  <td>{data.cost}</td>
+                  <td>{data.amountinstock}</td>
+                  <td>
+                    <button
+                      className="Button"
+                      onClick={() =>
+                        setMagic({
+                          ID: data.ID,
+                          category: data.category,
+                          name: data.name,
+                          size: data.size,
+                          price: data.price,
+                          cost: data.cost,
+                          amountinstock: data.amountinstock,
+                        })
+                      }
+                    >
+                      Update
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      className="Button"
+                      onClick={() => handleDelete(data.ID)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </table>
+          </div>
+        </div>
+
+        {/* For Data Update */}
+        {magic && (
+          <div className="App">
+            <div className="All-Update-Area">
+              <button className="Button" onClick={() => setMagic(null)}>
+                Close
+              </button>
+
+              <div className="Update">
+                <label className="Update-Label">Category</label>
+                <input
+                  type="text"
+                  className="Update-Text"
+                  value={magic.category}
+                  onChange={(e) =>
+                    setMagic({ ...magic, category: e.target.value })
+                  }
+                />
+              </div>
+
+              <div className="Update">
+                <label className="Update-Label">Name</label>
+                <input
+                  type="text"
+                  className="Update-Text"
+                  value={magic.name}
+                  onChange={(e) => setMagic({ ...magic, name: e.target.value })}
+                />
+              </div>
+
+              <div className="Update">
+                <label className="Update-Label">Size</label>
+                <select
+                  className="Update-Text"
+                  value={magic.size}
+                  onChange={(e) => setMagic({ ...magic, size: e.target.value })}
+                >
+                  <option> N/A </option>
+                  <option> Small </option>
+                  <option> Medium </option>
+                  <option> Large </option>
+                </select>
+              </div>
+
+              <div className="Update">
+                <label className="Update-Label">Price</label>
+                <input
+                  type="text"
+                  className="Update-Text"
+                  value={magic.price}
+                  onChange={(e) =>
+                    setMagic({ ...magic, price: e.target.value })
+                  }
+                />
+              </div>
+
+              <div className="Update">
+                <label className="Update-Label">Cost</label>
+                <input
+                  type="text"
+                  className="Update-Text"
+                  value={magic.cost}
+                  onChange={(e) => setMagic({ ...magic, cost: e.target.value })}
+                />
+              </div>
+
+              <div className="Update">
+                <label className="Update-Label">Amount in Stock</label>
+                <input
+                  type="text"
+                  className="Update-Text"
+                  value={magic.amountinstock}
+                  onChange={(e) =>
+                    setMagic({ ...magic, amountinstock: e.target.value })
+                  }
+                />
+              </div>
+
+              <button
+                className="Button"
+                onClick={() => updateToDatabase(magic)}
+              >
+                {" "}
+                Update{" "}
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
     </React.Fragment>
   );
 }
